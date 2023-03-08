9b869d64da0d6fe13d6b9ca8eb0e5b2db65be6ce
Younes Bouchamma <bouchamma.younes@gmail.com>
2023-03-08 21:41:52 +0100

    yelp2

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_07_202056) do
-  create_table "restaurants", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
-    t.integer "phone_number"
-    t.string "category"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "reviews", force: :cascade do |t|
-    t.string "content"
-    t.integer "rating"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.0].define(version: 0) do
 end
